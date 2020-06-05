d88aaf6c24f3428fd84611e5c8478eb26796fad0
JozsefVass <joco.udv@gmail.com>
2020-06-05 13:16:45 +0100

    Backend api is done.

---
+++ routes/api/items.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const Item = require("../../models/Item");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then((items) => res.status(200).json({ items }));
+});
+
+router.post("/", (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+  });
+  item.save().then((item) => {
+    res.status(200).json({ item });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) =>
+      item.remove().then(() => res.status(201).json({ success: true }))
+    )
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
